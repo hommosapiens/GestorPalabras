0848cc0bc829f7972170ba8aefad7acf3a9ae038
Pedro <pedrotrabajos22@gmail.com>
2024-02-09 17:23:11 +0100

    Arreglado palíndromos con tilde.

--- src/main/java/com/mycompany/gestorpalabras/GestorPalabras.java
+++ src/main/java/com/mycompany/gestorpalabras/GestorPalabras.java
@@ -3,7 +3,7 @@ package com.mycompany.gestorpalabras;
 public class GestorPalabras {
 
     public boolean esPalindromo(String palabra) {
-        String palabraFormateada = palabra.replaceAll("\\s+", "").toLowerCase();
+        String palabraFormateada = palabra.toLowerCase().replaceAll("\\s+", "").replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú", "u");
 
         for (int i = 0; i < palabraFormateada.length() / 2; i++) {
             if (palabraFormateada.charAt(i) != palabraFormateada.charAt(palabraFormateada.length() - i - 1)) {
--- src/test/java/GestorPalabrasTest.java
+++ src/test/java/GestorPalabrasTest.java
@@ -1,9 +1,32 @@
-/**
- * ID PONIENDO VUESTROS NOMBRES!!
- * @author Juan Pedro Rodriguez Aranda
- */
+
+import com.mycompany.gestorpalabras.GestorPalabras;
+import static junit.framework.TestCase.*;
+import org.junit.Test;
+
 public class GestorPalabrasTest {
     
+    @Test
+    public void palindromoBasico() {
+        GestorPalabras gest = new GestorPalabras();
+        assertTrue(gest.esPalindromo("oso") );
+        assertTrue(gest.esPalindromo("oso") );
+        assertFalse(gest.esPalindromo("cáliz") );
+        assertFalse(gest.esPalindromo("sílice") );
+    }
+    
+    @Test
+    public void palindromoTilde() {
+        GestorPalabras gest = new GestorPalabras();
+        assertTrue(gest.esPalindromo("dábale arróz a la zorrá el abad") );
+        assertTrue(gest.esPalindromo("allí ves sévilla") );
+    }
+    
+    @Test
+    public void palindromoConEspacios() {
+        GestorPalabras gest = new GestorPalabras();
+        assertTrue(gest.esPalindromo("  Somos    o no somos") );
+        assertTrue(gest.esPalindromo(" Anita  lava   la  tina  ") );
+    }
 
     
 }
