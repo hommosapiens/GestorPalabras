6118db024d4ac530c863f9bfe9d063d0055bb60d
Santi <santiago.pastor@educa.madrid.org>
2024-02-16 16:58:26 +0100

    arreglado el método contarVocales y algunos cambios estéticos

--- src/test/java/GestorPalabrasTest.java
+++ src/test/java/GestorPalabrasTest.java
@@ -8,41 +8,38 @@ public class GestorPalabrasTest {
     @Test
     public void palindromoBasico() {
         GestorPalabras gest = new GestorPalabras();
-        assertTrue(gest.esPalindromo("oso") );
-        assertTrue(gest.esPalindromo("oso") );
-        assertFalse(gest.esPalindromo("cáliz") );
-        assertFalse(gest.esPalindromo("sílice") );
+        assertTrue(gest.esPalindromo("oso"));
+        assertFalse(gest.esPalindromo("cáliz"));
+        assertFalse(gest.esPalindromo("sílice"));
     }
     
     @Test
     public void palindromoTilde() {
         GestorPalabras gest = new GestorPalabras();
-        assertTrue(gest.esPalindromo("dábale arróz a la zorrá el abad") );
-        assertTrue(gest.esPalindromo("allí ves sévilla") );
+        assertTrue(gest.esPalindromo("dábale arróz a la zorrá el abad"));
+        assertTrue(gest.esPalindromo("allí ves sévilla"));
     }
     
     @Test
     public void palindromoConEspacios() {
         GestorPalabras gest = new GestorPalabras();
-        assertTrue(gest.esPalindromo("  Somos    o no somos") );
-        assertTrue(gest.esPalindromo(" Anita  lava   la  tina  ") );
+        assertTrue(gest.esPalindromo("  Somos    o no somos"));
+        assertTrue(gest.esPalindromo(" Anita  lava   la  tina  "));
     }
-     @Test
+    
+    @Test
     public void invertirLaPalabra() {
         GestorPalabras gest = new GestorPalabras();
         assertEquals("aloh", gest.invertirPalabra("hola"));
         assertEquals("ALoH", gest.invertirPalabra("HoLA"));
     }
     
-  @Test
+    @Test
     public void contarVocalesTest() {
         GestorPalabras gest = new GestorPalabras();
         assertEquals(2, gest.contarVocales("díos"));
-     
-
+        assertEquals(2, gest.contarVocales("madre"));
+        assertEquals(2, gest.contarVocales("hábil"));
     }
-    
+   
 }
-
-    
-
